7dbb21c5f8e7a3847ecabc133cb4bf7167152ba7
Artem Vasylenko <artem.vasylenko@bnine.com>
2023-03-28 13:30:25 -0400

    add domain entities

---
+++ SpeedyAir/SpeedyAir.Domain/Flight/Flight.cs
@@ -0,0 +1,22 @@
+﻿namespace SpeedyAir.Domain;
+
+public class Flight
+{
+    public int Id { get; set; }
+
+    public int FlightNumber { get; set; }
+    
+    public string AirlineDesignator { get; set; }
+    
+    public string OriginCity { get; set; }
+
+    public string OriginAirportCode { get; set; }
+
+    public string DestinationCity { get; set; }
+    
+    public string DestinationAirportCode { get; set; }
+    
+    public int DepartureDay { get; set; }
+    
+    public int MaxAmountOfBoxes { get; set; }
+}
---
+++ SpeedyAir/SpeedyAir.Domain/Order/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace SpeedyAir.Domain.Order;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public string OrderIdentifier { get; set; }
+
+    public Flight Flight { get; set; }
+}
