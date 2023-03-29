068a810f07b7970b1b2fe404eca3694251f8dfe9
Artem Vasylenko <artem.vasylenko@bnine.com>
2023-03-28 23:28:16 -0400

    remove redundant comments

--- SpeedyAir/SpeedyAir.Application/AggregateRoots/Order/Commands/AddOrdersCommand.cs
+++ SpeedyAir/SpeedyAir.Application/AggregateRoots/Order/Commands/AddOrdersCommand.cs
@@ -3,7 +3,6 @@ using SpeedyAir.Application.AggregateRoots.Order.Models;
 
 namespace SpeedyAir.Application.AggregateRoots.Order.Commands;
 
-// Save to db
 public class AddOrdersCommand : IRequest<List<int>>
 {
     public List<AddOrderViewModel> Orders { get; set; }
--- SpeedyAir/SpeedyAir.Application/AggregateRoots/Order/Commands/ScheduleOrdersCommand.cs
+++ SpeedyAir/SpeedyAir.Application/AggregateRoots/Order/Commands/ScheduleOrdersCommand.cs
@@ -3,7 +3,6 @@ using SpeedyAir.Application.AggregateRoots.Order.Models;
 
 namespace SpeedyAir.Application.AggregateRoots.Order.Commands;
 
-// take saved orders from db by ids and schedule using available flights
 public class ScheduleOrdersCommand : IRequest<List<OrderViewModel>>
 {
     public List<int> OrderIds { get; set; }
